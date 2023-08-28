6eece3b45d85ae3a5a27fb4c9b82feefef7e296e
JM Bae <jungman82@gmail.com>
2023-08-28 10:38:24 +0900

    데이터베이스 PostgreSQL 지정
    CREATE ROLE educa WITH LOGIN PASSWORD '1234';
    
    CREATE DATABASE educa_db OWNER educa;

--- educa/settings.py
+++ educa/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "educa_db",
+        "USER": "educa",
+        "PASSWORD": "1234",
     }
 }
 
