f5be5d61a24873d3e73d5ca4c451b4dc2c454303
JM Bae <jungman82@gmail.com>
2023-08-28 15:09:26 +0900

    커스텀 필드 추가 OrderField ( 마이그레이션 기본값 설정 프롬프트 0,1 지정 )

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from courses.fields import OrderField
+
 
 # Create your models here.
 class Subject(models.Model):
@@ -41,9 +43,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -55,6 +61,10 @@ class Content(models.Model):
                                      limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
