856bdb41ea21ea3fe9f84ebc29b8812181f4360e
JM Bae <jungman82@gmail.com>
2023-08-31 15:01:30 +0900

    django-debug-toolbar 설정

--- educa/settings.py
+++ educa/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
     'students.apps.StudentsConfig',
     'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +136,7 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
