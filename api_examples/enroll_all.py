0ed2d5083c8f09aae7a74d23f30273ba0f765aa8
JM Bae <jungman82@gmail.com>
2023-09-05 10:10:56 +0900

    course enroll api 실행 테스트

--- api_examples/enroll_all.py
+++ api_examples/enroll_all.py
@@ -7,3 +7,17 @@ courses = r.json()
 
 available_courses = ', '.join([course['title'] for course in courses])
 print(f'Available courses: {available_courses}')
+
+username = 'jmbae'
+password = 'cge8fye*vqr2btz1WRD'
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+
+    r = requests.post(f'{base_url}courses/{course_id}/enroll/', auth=(username, password))
+
+    if r.status_code == 200:
+        print(f'Successfully enrolled in {course_title}')
+    else:
+        print(f'error {r.status_code}:${r.text}')
