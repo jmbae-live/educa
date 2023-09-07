dfdc8f606dfbbe0cc326241a2e831d1389ed2c01
JM Bae <jungman82@gmail.com>
2023-09-07 14:00:50 +0900

    Cache 설정

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -1,6 +1,7 @@
 import os
 
 from .base import *
+
 DEBUG = False
 ADMINS = [
     ('jmbae', 'jmbae@live.com'),
@@ -16,3 +17,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
